132442985b8950237bcffaff3f1880e8ec6d6556
Goldenframe <ninakya@mail.ru>
2025-04-09 20:16:51 +0300

    Изменила ссылку на бэк

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: '/',
     plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://api.dom-ban-na-drovah.ru', 
-                changeOrigin: true,       
-                secure: true,            
-                rewrite: (path) => path.replace(/^\/api/, ''), 
-                headers: {
-                    'X-Forwarded-Proto': 'https'
-                }
-            }
-        },
-        historyApiFallback: true,
-    },
 })
