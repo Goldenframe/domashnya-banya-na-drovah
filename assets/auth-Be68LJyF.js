291f3a084a26f5e72f5db2852cc41f92a50cbbe2
Goldenframe <ninakya@mail.ru>
2025-04-09 20:03:00 +0300

    Updates

--- assets/auth-Be68LJyF.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-83Tp2EN1.js";function f(){const[a,o]=t.useState(""),[u,r]=t.useState(""),[i,e]=t.useState(!1);return{message:a,showMessage:s=>{o(s),e(!0),setTimeout(()=>e(!1),7e3),setTimeout(()=>o(""),8e3)},error:u,showError:s=>{r(s),e(!0),setTimeout(()=>e(!1),7e3),setTimeout(()=>r(""),8e3)},isVisible:i}}export{f as u};
