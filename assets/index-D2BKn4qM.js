37abedc5e6bc5e336159e393f2fd024f64e791bb
Goldenframe <ninakya@mail.ru>
2025-04-11 16:24:38 +0300

    Updates

--- assets/index-D2BKn4qM.js
+++ assets/index-D2BKn4qM.js
@@ -1,4 +1,4 @@
-import{h as bn,i as Ne}from"./index-C2fblbPb.js";/*!
+import{h as bn,i as Ne}from"./index-CdRdZjSu.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
