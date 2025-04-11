668b9b3c3987f176f4efc719acd016c58801532c
Goldenframe <ninakya@mail.ru>
2025-04-11 16:12:51 +0300

    Изменила стили

---
+++ client/dist/assets/auth-Bo2pOuGR.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-DZZxIip0.js";function f(){const[a,o]=t.useState(""),[u,r]=t.useState(""),[i,e]=t.useState(!1);return{message:a,showMessage:s=>{o(s),e(!0),setTimeout(()=>e(!1),7e3),setTimeout(()=>o(""),8e3)},error:u,showError:s=>{r(s),e(!0),setTimeout(()=>e(!1),7e3),setTimeout(()=>r(""),8e3)},isVisible:i}}export{f as u};
